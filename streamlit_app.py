fcda9576f8397f5eac767957589aa587d1c3cbd6
zyanca <zyancaaaap18@gmail.com>
2026-05-22 10:30:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Simulasi Titrasi & Kadar pH")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
